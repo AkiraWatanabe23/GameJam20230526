c16ea30dbbee7846543d43ed67e4c13662098eb2
Akira Watanabe <a0323w@gmail.com>
2023-05-26 15:03:15 +0900

    [update] 一掃アイテムやってます

--- Assets/Shigetomi/PlayerAttack.cs
+++ Assets/Shigetomi/PlayerAttack.cs
@@ -5,6 +5,7 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] GameObject Bullet;
     [SerializeField] Transform[] _muzzles = new Transform[2];
     [SerializeField] private BulletType _type = BulletType.Normal;
+    [SerializeField] private RandomSweep _sweep = default;
 
     private bool _isSweepAway = false;
     private bool _isPowerUp = false;
@@ -30,6 +31,10 @@ public class PlayerAttack : MonoBehaviour
         }
         else if (Input.GetMouseButtonDown(1) && _isSweepAway)
         {
+            if (_sweep)
+            {
+                _sweep.SettingPos();
+            }
             _isSweepAway = false;
         }
 
