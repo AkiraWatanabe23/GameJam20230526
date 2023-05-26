140620aaabc5eab9b2a9fe4441a0a2a77600c913
Akira Watanabe <a0323w@gmail.com>
2023-05-26 10:47:36 +0900

    [add] GameManager, PlayerMove

---
+++ Assets/Watanabe/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using Constants;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private SceneNames _scene = default;
+
+    public void LoadToScene()
+    {
+        SceneManager.LoadScene(Consts.Scenes[_scene]);
+    }
+}
---
+++ Assets/Watanabe/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameManager _manager = default;
+    [SerializeField] private Text _timerText = default;
+
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        _timerText.text = _manager.Timer.ToString("F0");
+    }
+}
