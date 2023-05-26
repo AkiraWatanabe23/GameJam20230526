6a34606d4943a07e47dba9bd078c8eda20afbdc8
Octorok192 <awmcco893@i.softbank.jp>
2023-05-26 11:56:36 +0900

    [add] 弾の移動

---
+++ Assets/Shigetomi/PlayerAttack.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    //’e‚Ì‘¬“x
+    [SerializeField] float m_initialSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, Time.deltaTime * m_initialSpeed); 
+    }
+}
